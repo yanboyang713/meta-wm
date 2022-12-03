c7e91eb9b193f9dc6875572666f08ec4bb5de629
Boyang Yan <yanboyang713@gmail.com>
2022-12-03 15:42:20 -0500

    add lock

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,5 +6,10 @@ feh --bg-fill /opt/meta-wm/scripts/wallpaper.png &
 xset r rate 200 50 &
 picom &
 
+# Trigger screensaver after 10 minutes of inactivity
+xset s on
+xset s 300 5
+xss-lock -n /usr/lib/xsecurelock/dimmer -l -- xsecurelock &
+
 /opt/meta-wm/scripts/bar.sh &
 while type dwm >/dev/null; do dwm && continue || break; done
